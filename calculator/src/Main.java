acf07e23a264c1c1db46bb9655a76449d86219cb
404_updates <115992669+maksiiiim20@users.noreply.github.com>
2023-12-09 03:11:03 +0100

    Update Main.java

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+aimport java.util.Scanner;
 public class KanonicznaPostacKwadratowej {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
